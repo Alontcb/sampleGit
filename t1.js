d0cfefa7a2527c6f575b5ad085ac0ef84f6b0d20
nir hadari <n.hada4@college.labs>
2025-12-23 14:24:12 +0200

    nir commit

--- t1.js
+++ t1.js
@@ -1,2 +1,9 @@
-while(true)
-    console.log("hello world")
+const greet=(name)=>{
+    console.log(`hello ${name}`);
+}
+const greet2=(name1, name2)=>{
+    console.log(`hello ${name} , ${name2}`);
+}
+
+greet("nir")
+greet2("nir", "alon")
