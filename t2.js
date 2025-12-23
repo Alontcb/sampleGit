560260e5d7f969d11d5bdfb560d1513c67140520
Alon Shraybman <alons@college.labs>
2025-12-23 14:15:01 +0200

    finising alon task

---
+++ t2.js
@@ -0,0 +1,7 @@
+const add = (x,y) =>x+y
+const minus = (x,y) =>x-y
+const mult = (x,y) =>x*y
+const devied = (x,y) =>y==0?null:x/y
+const power = (x,y) =>x**y
+
+export {power,mult,minus,devied,add}
